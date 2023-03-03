e08fe6062c0982110b68c50cf01816e434aa09c2
Lucas Barrena <lucasbarrena98@gmail.com>
2023-03-03 15:01:08 -0300

    add new test that is similar but passing

--- test/socket.js
+++ test/socket.js
@@ -529,6 +529,25 @@ test('bind while closing', function (t) {
   }
 })
 
+test('different socket binds to same host and port', async function (t) {
+  t.plan(1)
+
+  const u = new UDX()
+  const a = u.createSocket()
+  const b = u.createSocket()
+
+  a.bind(0, '0.0.0.0')
+
+  try {
+    b.bind(a.address().port, '0.0.0.0')
+  } catch (error) {
+    t.is(error.code, 'EADDRINUSE')
+  }
+
+  await a.close()
+  await b.close()
+})
+
 test('different socket binds to specific host but same port', async function (t) {
   t.plan(1)
 
