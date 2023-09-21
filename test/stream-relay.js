c10e0d28411540ceae5198f5c2dcc037fb3dacdf
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-09-21 21:32:37 +0200

    Update `libudx` and add `stream.changeRemote()` (#9)

--- test/stream-relay.js
+++ test/stream-relay.js
@@ -37,3 +37,64 @@ test('relay, destroy immediately', function (t) {
 
   t.pass()
 })
+
+test('relay, change remote', function (t) {
+  t.plan(2)
+
+  const [a, b] = makeTwoStreams(t)
+  const [c, d] = makeTwoStreams(t)
+
+  c.relayTo(b)
+  b.relayTo(c)
+
+  a.once('data', async function (data) {
+    t.alike(data, b4a.from('hello world'))
+
+    await a.changeRemote(a.socket, d.id, d.socket.address().port)
+    await d.changeRemote(d.socket, a.id, a.socket.address().port)
+
+    b.destroy()
+    c.destroy()
+
+    a.once('data', function (data) {
+      t.alike(data, b4a.from('remote changed'))
+
+      a.destroy()
+      d.destroy()
+    })
+
+    d.write('remote changed')
+  })
+
+  d.write('hello world')
+})
+
+test('relay, change remote and destroy stream', function (t) {
+  t.plan(2)
+
+  const [a, b] = makeTwoStreams(t)
+  const [c, d] = makeTwoStreams(t)
+
+  c.relayTo(b)
+  b.relayTo(c)
+
+  a.once('data', async function (data) {
+    t.alike(data, b4a.from('hello world'))
+
+    const promises = [
+      a.changeRemote(a.socket, d.id, d.socket.address().port),
+      d.changeRemote(d.socket, a.id, a.socket.address().port)
+    ]
+
+    a.destroy()
+    b.destroy()
+    c.destroy()
+    d.destroy()
+
+    await Promise.allSettled(promises)
+
+    t.pass()
+  })
+
+  d.write('hello world')
+})
